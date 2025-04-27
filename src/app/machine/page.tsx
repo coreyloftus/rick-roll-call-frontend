8ddce5b662cccc920f723169be815aad0800e5ed
Corey Loftus <corey.loftus@syncsmart.io>
2025-04-27 14:24:21 -0400

    add section for audio test

--- src/app/machine/page.tsx
+++ src/app/machine/page.tsx
@@ -1,5 +1,8 @@
 'use client'
 import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
     // Accordion,
     // AccordionDetails,
     // AccordionSummary,
@@ -10,7 +13,7 @@ import {
 } from '@mui/material'
 // import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import { useState, useEffect } from 'react'
-import { geminiTest } from '../api/geminiCalls'
+import { geminiAudio, geminiTest } from '../api/geminiCalls'
 import { remark } from 'remark'
 import remarkToc from 'remark-toc'
 import ReactMarkdown from 'react-markdown'
@@ -21,6 +24,10 @@ const Page = () => {
     const [textValue, setTextValue] = useState('')
     const [geminiReply, setGeminiReply] = useState('reply will be here')
     const testAudioPath = '/audio/obi-wan.mp3'
+
+    const [voiceInputText, setVoiceInputText] = useState('')
+    const [audioUrl, setAudioUrl] = useState('')
+    const [audioBlob, setAudioBlob] = useState<Blob | null>(null)
 
     useEffect(() => {
         const darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
@@ -39,6 +46,18 @@ const Page = () => {
         console.log(replyText)
         setGeminiReply(processedMarkdown.toString())
     }
+
+    const handleAudioTextSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        const res = await geminiAudio(voiceInputText)
+        try {
+            setAudioUrl(res)
+            localStorage.setItem('geminiAudioUrl', JSON.stringify(res))
+        } catch (err) {
+            console.error('Error saving to localStorage', err)
+        }
+    }
+
     const badWordDict = ['fuck', 'shit', 'hell']
     return (
         <>
@@ -53,60 +72,114 @@ const Page = () => {
                     minHeight: '100vh'
                 }}
             >
-                <Typography variant='h4'>Welcome to Answering Machine</Typography>
-                <Typography variant='h5'>{'This is an AI voice generator for Twilio.'}</Typography>
                 <Box sx={{ border: 1, p: 2, borderRadius: 2, width: '80vw' }}>
-                    <Typography variant='h6'>Gemini Connection Test</Typography>
-                    <TextField
-                        id='ai-text-input'
-                        label='AI Text Input'
-                        variant='outlined'
-                        fullWidth
-                        placeholder='Enter your text here...'
-                        value={textValue}
-                        onChange={(e) => setTextValue(e.target.value)}
-                        error={badWordDict.includes(textValue)}
-                        helperText={badWordDict.includes(textValue) ? 'Please avoid using bad words.' : ''}
-                        // change color of input text to white if in dark mode
-                        sx={{
-                            input: { color: `${darkMode ? 'white' : 'black'}` },
-                            label: { color: `${darkMode ? 'white' : 'black'}` },
-                            '& .MuiOutlinedInput-root': {
-                                '& fieldset': {
-                                    borderColor: `${darkMode ? 'white' : 'black'}`
-                                },
-                                '&:hover fieldset': {
-                                    borderColor: `${darkMode ? 'gray' : 'darkgray'}`
-                                },
-                                '&.Mui-focused fieldset': {
-                                    borderColor: `${darkMode ? 'lightblue' : 'blue'}`
+                    <Typography variant='h4'>Welcome to Answering Machine</Typography>
+                    <Typography variant='h5'>{'This is an AI voice generator for Twilio.'}</Typography>
+                    <Accordion>
+                        <AccordionSummary>
+                            <Typography variant='h6'>Gemini Connection Test</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <TextField
+                                id='ai-text-input'
+                                label='AI Text Input'
+                                variant='outlined'
+                                fullWidth
+                                placeholder='Enter your text here...'
+                                value={textValue}
+                                onChange={(e) => setTextValue(e.target.value)}
+                                error={badWordDict.includes(textValue)}
+                                helperText={badWordDict.includes(textValue) ? 'Please avoid using bad words.' : ''}
+                                // change color of input text to white if in dark mode
+                                sx={{
+                                    input: { color: `${darkMode ? 'white' : 'black'}` },
+                                    label: { color: `${darkMode ? 'white' : 'black'}` },
+                                    '& .MuiOutlinedInput-root': {
+                                        '& fieldset': {
+                                            borderColor: `${darkMode ? 'white' : 'black'}`
+                                        },
+                                        '&:hover fieldset': {
+                                            borderColor: `${darkMode ? 'gray' : 'darkgray'}`
+                                        },
+                                        '&.Mui-focused fieldset': {
+                                            borderColor: `${darkMode ? 'lightblue' : 'blue'}`
+                                        }
+                                    },
+                                    '& .MuiInputLabel-root': {
+                                        color: `${darkMode ? 'white' : 'black'}`
+                                    },
+                                    '& .MuiInputLabel-root.Mui-focused': {
+                                        color: `${darkMode ? 'lightblue' : 'blue'}`
+                                    },
+                                    '& .MuiInputBase-input::placeholder': {
+                                        color: `${darkMode ? 'gray' : 'darkgray'}`
+                                    }
+                                }}
+                            />
+                            <Button variant='contained' color='primary' onClick={(e) => handleTextSubmit(e)}>
+                                Submit
+                            </Button>
+                            <Box sx={{ mt: 6, width: '100%' }}>
+                                <Typography variant='h6' sx={{ textAlign: 'left' }}>
+                                    Gemini Reply:
+                                </Typography>
+                                <Box sx={{ ml: 4 }}>
+                                    <ReactMarkdown>{geminiReply}</ReactMarkdown>
+                                </Box>
+                            </Box>
+                        </AccordionDetails>
+                    </Accordion>
+
+                    <Accordion>
+                        <AccordionSummary>
+                            <Typography variant='h6'>Audio Player</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography variant='body1'>
+                                {
+                                    'Input your text and click submit to generate the audio. The audio will be played below.'
                                 }
-                            },
-                            '& .MuiInputLabel-root': {
-                                color: `${darkMode ? 'white' : 'black'}`
-                            },
-                            '& .MuiInputLabel-root.Mui-focused': {
-                                color: `${darkMode ? 'lightblue' : 'blue'}`
-                            },
-                            '& .MuiInputBase-input::placeholder': {
-                                color: `${darkMode ? 'gray' : 'darkgray'}`
-                            }
-                        }}
-                    />
-                    <Button variant='contained' color='primary' onClick={(e) => handleTextSubmit(e)}>
-                        Submit
-                    </Button>
-                    <Box sx={{ mt: 6, width: '100%' }}>
-                        <Typography variant='h6' sx={{ textAlign: 'left' }}>
-                            Gemini Reply:
-                        </Typography>
-                        <Box sx={{ ml: 4 }}>
-                            <ReactMarkdown>{geminiReply}</ReactMarkdown>
-                        </Box>
-                    </Box>
-                    <Box>
-                        <AudioPlayer audioUrl={testAudioPath} />
-                    </Box>
+                            </Typography>
+                            <TextField
+                                id='voice-input-text'
+                                label='Voice Input Text'
+                                variant='outlined'
+                                placeholder='Enter your text here...'
+                                value={voiceInputText}
+                                onChange={(e) => setVoiceInputText(e.target.value)}
+                                sx={{
+                                    input: { color: `${darkMode ? 'white' : 'black'}` },
+                                    label: { color: `${darkMode ? 'white' : 'black'}` },
+                                    '& .MuiOutlinedInput-root': {
+                                        '& fieldset': {
+                                            borderColor: `${darkMode ? 'white' : 'black'}`
+                                        },
+                                        '&:hover fieldset': {
+                                            borderColor: `${darkMode ? 'gray' : 'darkgray'}`
+                                        },
+                                        '&.Mui-focused fieldset': {
+                                            borderColor: `${darkMode ? 'lightblue' : 'blue'}`
+                                        }
+                                    },
+                                    '& .MuiInputLabel-root': {
+                                        color: `${darkMode ? 'white' : 'black'}`
+                                    },
+                                    '& .MuiInputLabel-root.Mui-focused': {
+                                        color: `${darkMode ? 'lightblue' : 'blue'}`
+                                    },
+                                    '& .MuiInputBase-input::placeholder': {
+                                        color: `${darkMode ? 'gray' : 'darkgray'}`
+                                    }
+                                }}
+                            ></TextField>
+                            <Button variant='contained' color='primary' onClick={(e) => handleAudioTextSubmit(e)}>
+                                Submit
+                            </Button>
+                            <Box>
+                                <AudioPlayer audioUrl={testAudioPath} />
+                            </Box>
+                        </AccordionDetails>
+                    </Accordion>
                 </Box>
             </Box>
         </>
